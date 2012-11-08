f3e23a0eac6b17b48c41ad3029ac037effa13ac8
Sergey Babinsky <shimopus@gmail.com>
2012-11-09 02:18:28 +0400

    Innova Task3

--- Innova/light/light.js
+++ Innova/light/light.js
@@ -1,44 +1,156 @@
 (function (Raphael, $) {
     "use strict"
 
+    function Coordinates(x, y, width, height) {
+        this.x = x || 0;
+        this.y = y || 0;
+        this.width = width || 0;
+        this.height = height || 0;
+
+        this.fromElement = function(element) {
+            var currentOffset = element.offset();
+            this.x = currentOffset.left;
+            this.y = currentOffset.top;
+            this.width = element.width();
+            this.height = element.height();
+
+            return this;
+        }
+    }
+
+    function Canvas() {
+        this.coordinates = new Coordinates();
+        this.path = {
+            x: 0, y: 0,
+            x1: 0, y1: 0,
+            x2: 0, y2: 0
+        };
+    }
+
+    function getCanvasAttributes(start, end) {
+        var startCoords = new Coordinates().fromElement(start);
+        var endCoords = new Coordinates().fromElement(end);
+        var canvasPath;
+
+
+        if (startCoords.x < endCoords.x && startCoords.y < endCoords.y
+            ||
+            startCoords.x >= endCoords.x && startCoords.y >= endCoords.y) {
+            canvasPath = {
+                x1: endCoords.x + endCoords.width, y1: endCoords.y,
+                x2: endCoords.x, y2: endCoords.y + endCoords.height
+            };
+            //Start - in left top  End - in right bottom
+            if (startCoords.x < endCoords.x && startCoords.y < endCoords.y) {
+                canvasPath.x = startCoords.x + startCoords.width;
+                canvasPath.y = startCoords.y + startCoords.height;
+            } else
+            //Start - in right bottom End - in left top
+            {
+                canvasPath.x = startCoords.x;
+                canvasPath.y = startCoords.y;
+            }
+        } else
+        if (startCoords.x < endCoords.x && startCoords.y >= endCoords.y
+            ||
+            startCoords.x >= endCoords.x && startCoords.y < endCoords.y) {
+            canvasPath = {
+                x1: endCoords.x, y1: endCoords.y,
+                x2: endCoords.x + endCoords.width, y2: endCoords.y + endCoords.height
+            };
+
+            //Start - in left bottom End - in right top
+            if (startCoords.x < endCoords.x && startCoords.y >= endCoords.y) {
+                canvasPath.x = startCoords.x + startCoords.width;
+                canvasPath.y = startCoords.y;
+            } else
+            //Start - in right top End - in left bottom
+            {
+                canvasPath.x = startCoords.x;
+                canvasPath.y = startCoords.y + startCoords.height;
+            }
+        }
+
+        var canvas = new Canvas();
+        canvas.coordinates = getCanvasCoordinatesByPath(canvasPath);
+        canvas.path = makeCanvasPathRelative(canvasPath, canvas.coordinates);
+
+        return canvas;
+    }
+
+    function getCanvasCoordinatesByPath(canvasPath) {
+        var coordinates = new Coordinates();
+        coordinates.x = Math.min(canvasPath.x, canvasPath.x1, canvasPath.x2);
+        coordinates.y = Math.min(canvasPath.y, canvasPath.y1, canvasPath.y2);
+        coordinates.width = Math.max(canvasPath.x, canvasPath.x1, canvasPath.x2) - coordinates.x;
+        coordinates.height = Math.max(canvasPath.y, canvasPath.y1, canvasPath.y2) - coordinates.y;
+
+        return coordinates;
+    }
+
+    function makeCanvasPathRelative(canvasPath, canvasCoords) {
+        var path = {};
+        path.x = canvasPath.x - canvasCoords.x;
+        path.x1 = canvasPath.x1 - canvasCoords.x;
+        path.x2 = canvasPath.x2 - canvasCoords.x;
+        path.y = canvasPath.y - canvasCoords.y;
+        path.y1 = canvasPath.y1 - canvasCoords.y;
+        path.y2 = canvasPath.y2 - canvasCoords.y;
+
+        return path;
+    }
+
+    var paper;
+
+    function initializeCanvas(start, end) {
+        var canvas = getCanvasAttributes(start, end);
+
+        drawLight(canvas);
+    }
+
+    function reinitializeCanvas(start, end) {
+        paper.remove();
+        initializeCanvas(start, end);
+    }
+
+    function drawLight(canvas) {
+        paper = Raphael(
+            canvas.coordinates.x, canvas.coordinates.y,
+            canvas.coordinates.width, canvas.coordinates.height);
+
+        paper.path(Raphael.format("M{0},{1}L{2},{3}L{4},{5}L{6},{7}z",
+            canvas.path.x, canvas.path.y,
+            canvas.path.x1, canvas.path.y1,
+            canvas.path.x2, canvas.path.y2,
+            canvas.path.x, canvas.path.y))
+            .attr({
+                fill: "90-#fff:5-#000:95",
+                "fill-opacity": "0.5",
+                stroke: "none",
+                opacity: "0.05"
+            });
+    }
+
+    function initDnDAndResize(start, end) {
+        var reinit = function () {
+            reinitializeCanvas(start, end);
+        };
+
+        $.each([start, end], function (i, element) {
+            element.draggable({
+                drag: reinit,
+                stop: reinit
+            }).resizable({
+                    resize: reinit,
+                    stop: reinit
+                });
+        });
+    }
+
     $(function() {
-        initializeCanvas("canvas", "start", "end");
+        var start = $("#start");
+        var end = $("#end");
+        initializeCanvas(start, end);
+        initDnDAndResize(start, end);
     });
-
-    function getCanvasAttributes(startElementId, endElementId) {
-        var start = $("#" + startElementId);
-        var end = $("#" + endElementId);
-
-        var startCoords = start.offset();
-        var endCoords = end.offset();
-        var canvasCoords = {};
-
-        if (startCoords.left > endCoords.left) {
-            canvasCoords.left = endCoords.left;
-        } else {
-            canvasCoords.left = startCoords.left;
-        }
-        canvasCoords.width = Math.abs(startCoords.left - endCoords.left);
-
-        if (startCoords.top > endCoords.top) {
-            canvasCoords.top = endCoords.top;
-        } else {
-            canvasCoords.top = startCoords.top;
-        }
-        canvasCoords.height = Math.abs(startCoords.top - endCoords.top);
-
-        return canvasCoords;
-    }
-
-    function initializeCanvas(canvasId, startElementId, endElementId) {
-        var canvasCoords = getCanvasAttributes(startElementId, endElementId);
-
-        drawLight("#" + canvasId, canvasCoords);
-    }
-
-    function drawLight(canvasId, canvasCoords) {
-        var paper = Raphael(canvasCoords.top, canvasCoords.left, canvasCoords.width, canvasCoords.height);
-
-        paper.ellipse(canvasCoords.top, canvasCoords.left, 100, 100).attr({fill: "r(.8,.8) white-green", stroke: "none"});
-    }
 })(Raphael, jQuery);
